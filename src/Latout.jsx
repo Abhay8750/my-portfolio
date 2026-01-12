80856c9194741fa55c7b3ba5c0484c302b7ee555
unknown <abhaypa875@gmail.com>
2026-01-12 19:51:48 +0530

    Fix layout duplicate components

--- src/Latout.jsx
+++ src/Latout.jsx
@@ -1,12 +1,10 @@
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
-import { Outlet } from "react-router-dom";
 
 function Layout() {
   return (
     <>
       <Nav />
-      <Outlet />
       <Footer />
     </>
   );
